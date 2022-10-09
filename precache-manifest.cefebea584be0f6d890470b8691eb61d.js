b6f006d05578817a1691ed8c6946564d7a3e02fa
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-10-09 17:35:35 +0330

    Updates

---
+++ precache-manifest.cefebea584be0f6d890470b8691eb61d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e15049c40c961cadfb9",
+    "url": "/ShopLandingPage/static/css/main.c2ca7916.chunk.css"
+  },
+  {
+    "revision": "8e15049c40c961cadfb9",
+    "url": "/ShopLandingPage/static/js/main.8e15049c.chunk.js"
+  },
+  {
+    "revision": "88cd8b651f2587c42175",
+    "url": "/ShopLandingPage/static/css/1.dd458358.chunk.css"
+  },
+  {
+    "revision": "88cd8b651f2587c42175",
+    "url": "/ShopLandingPage/static/js/1.88cd8b65.chunk.js"
+  },
+  {
+    "revision": "5ec3dc138801337343e4",
+    "url": "/ShopLandingPage/static/js/runtime~main.5ec3dc13.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ShopLandingPage/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ShopLandingPage/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ShopLandingPage/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ShopLandingPage/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "df5037e6706748e886d3b3ac94bd963f",
+    "url": "/ShopLandingPage/static/media/IRANSansX-Bold.df5037e6.woff"
+  },
+  {
+    "revision": "f10ab95fd7eae5e3dfe5e1c84c2bb8de",
+    "url": "/ShopLandingPage/static/media/IRANSansX-DemiBold.f10ab95f.woff"
+  },
+  {
+    "revision": "e4155041a3df304f61dd2fcd2a901135",
+    "url": "/ShopLandingPage/static/media/IRANSansX-Medium.e4155041.woff"
+  },
+  {
+    "revision": "e076ef3a3db0202ec80889544eafce7a",
+    "url": "/ShopLandingPage/static/media/IRANSansX-Regular.e076ef3a.woff"
+  },
+  {
+    "revision": "df025fdbb736407c28562fc20b2744e1",
+    "url": "/ShopLandingPage/static/media/IRANSansX-Light.df025fdb.woff"
+  },
+  {
+    "revision": "0d74920d6e7ad7fecd130b468899b707",
+    "url": "/ShopLandingPage/static/media/IRANSansX-Thin.0d74920d.woff"
+  },
+  {
+    "revision": "f84c48ce48a47538e1015bda819e3cdd",
+    "url": "/ShopLandingPage/index.html"
+  }
+];
